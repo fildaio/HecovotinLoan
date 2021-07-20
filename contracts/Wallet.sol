90d924f460a2b7672f4a880e3dafeee31b3f375e
Yushin Si <siyushin@hotmail.com>
2021-07-20 10:29:55 +0800

    整理代码，可编译。

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
 import "./VotingStrategy.sol";
 import "./LoanStrategy.sol";
 import "./HTTokenInterface.sol";
@@ -10,6 +11,7 @@ import "./Global.sol";
 
 contract Wallet is AccessControl, Global {
 	using SafeMath for uint256;
+	using Math for uint256;
 
 	struct RedeemingState {
 		uint256 blockNumber;
@@ -24,6 +26,7 @@ contract Wallet is AccessControl, Global {
 	bool public isExist = false;
 
 	address private _owner;
+	uint256 private _decimals = 1e18;
 	uint256 private _minVoteAmount = 1e18;
 	uint256 private _denominator = 10000;
 	uint256 private _borrowRate = 8000;
@@ -32,7 +35,8 @@ contract Wallet is AccessControl, Global {
 	uint256 private _bonusRateForLiquidater = 3;
 	address private _firstLiquidater;
 	address private _secondLiquidater;
-	address private _admin = 0x123;
+	address private _admin = address(0x123);
+	uint256 private _exchangeRate = 1e18;
 	address payable private _deployedVoteContract = payable(address(0x123));
 	address payable private _deployedLoanContract = payable(address(0x123));
 	HTTokenInterface private _HTT = HTTokenInterface(address(0x123));
@@ -40,11 +44,12 @@ contract Wallet is AccessControl, Global {
 	// Events
 	event voteEvent(address voter, uint256 pid, uint256 amount);
 	event burnHTTEvent(address voter, uint256 amount);
-	event claimEvent(caller, pid, amount);
+	event claimEvent(address caller, uint256 pid, uint256 amount);
 	event withdrawEvent(address voter, uint256 pid, uint256 amount);
 	event quickWithdrawalEvent(address voter, uint256 amount);
 	event revokeEvent(address voter, uint256 pid, uint256 amount);
 	event liquidateEvent(address voter, uint256 amount);
+	event repayEvent(address voter, uint256 pid, uint256 amount);
 
 	constructor(address owner, address admin) {
 		_setupRole(ADMIN_ROLE, msg.sender);
@@ -85,14 +90,13 @@ contract Wallet is AccessControl, Global {
 
 		uint256 newBalance = _HTT.balance(address(this));
 
-		require(newBalance.sub(oldBalance) == integerAmount));
+		require(newBalance.sub(oldBalance) == integerAmount);
 
 		uint256 mintResult = loanContract.mint(integerAmount);
 
-		require(mintResult * ExchangeRateStored / 1e18 == integerAmount);
+		require(mintResult.mul(_exchangeRateStored()).div(_decimals) == integerAmount);
 
 		emit voteEvent(caller, pid, integerAmount);
-
 	}
 
 	function getBorrowLimit() public returns (uint256) {
@@ -108,9 +112,7 @@ contract Wallet is AccessControl, Global {
 
 		uint256 borrowed = loanContract.borrow(borrowAmount);
 
-		// TODO: Which one is better?
-		// require(borrowed == borrowAmount);
-		// require(borrowed > 0);
+		require(borrowed > 0);
 
 		payable(msg.sender).transfer(borrowed);
 	}
@@ -119,7 +121,7 @@ contract Wallet is AccessControl, Global {
 		require(votingContract.pendingReward(pid) > 0, "No rewards to claim.");
 
 		uint256 oldBalance = address(this).balance;
-		require(votingContract.claimReward(pid))
+		require(votingContract.claimReward(pid));
 
 		uint256 newBalance = address(this).balance;
 		uint256 rewardToClaim = newBalance.sub(oldBalance);
@@ -149,64 +151,30 @@ contract Wallet is AccessControl, Global {
 		return votingContract.revokingInfo(address(this), pid);
 	}
 
-	// function revokingDone(uint256 pid) public returns (bool) {
-	// 	uint256 lockingEndTime;
-	// 	(, , lockingEndTime) = votingContract.revokingInfo(address(this), pid);
-	// 	bool withdrawable = isWithdrawable(pid);
-	// 	if (lockingEndTime < block.timestamp && withdrawable == true) {
-	// 		return true;
-	// 	} else {
-	// 		return false;
-	// 	}
-	// }
-
-
 	function isWithdrawable(uint256 pid) public returns (bool) {
 		return votingContract.isWithdrawable(address(this), pid);
 	}
 
-	function withdrawVoting(uint256 pid) public return (uint256 withdrawal) {
+	function withdrawVoting(uint256 pid) public returns (uint256 withdrawal) {
 		require(msg.sender == _owner);
 		withdrawal = _withdrawOrRepay(pid, false);
 		payable(msg.sender).transfer(address(this).balance);
-		emit withdrawEvent(msg.sender, pid, amount);
+		emit withdrawEvent(msg.sender, pid, withdrawal);
 	}
 
-	function withdrawAndRepay(uint256 pid) public return (uint256 withdrawal) {
+	function withdrawAndRepay(uint256 pid) public returns (uint256 withdrawal) {
 		require(msg.sender == _owner);
 		withdrawal = _withdrawOrRepay(pid, true);
 		payable(msg.sender).transfer(address(this).balance);
-		emit withdrawEvent(msg.sender, pid, amount);
+		emit withdrawEvent(msg.sender, pid, withdrawal);
 	}
 
 	function withdrawAndRepayAll() public {
 		require(msg.sender == _owner);
-		_withdrawAllVoting(true);
+		uint256 withdrawal = _withdrawAllVoting(true);
 		payable(msg.sender).transfer(address(this).balance);
-		emit withdrawEvent(msg.sender, pid, amount);
+		emit withdrawEvent(msg.sender, 99999, withdrawal);
 	}
-
-	// 负责从投票合约里把HT取出到钱包中。
-	// 第二个参数thenTransafer为true时可以在提取后直接把HT转到用户帐户中。
-	// 如果不这样做的话，那么在调用这个function成功的地方需要把已提取出的HT转到用户帐户中。这样方便批量从投票合约中提取，并一次性转帐给用户。
-	// function withdrawVoting(uint256 pid, bool thenTransafer) public returns (uint256 withdrawal) {
-	// 	require(msg.sender == _owner);
-	// 	require(isWithdrawable(pid) == true);
-	// 
-	// 	uint256 oldBalance = address(this).balance;
-	// 	bool done = votingContract.withdraw(pid);
-	// 	if (done == true) {
-	// 		uint256 newBalance = address(this).balance;
-	// 		withdrawal = newBalance.sub(oldBalance);
-	// 
-	// 		if (thenTransafer == true) {
-	// 			payable(msg.sender).transfer(withdrawal);
-	// 		}
-	// 	} else {
-	// 		revert(); //"Failed to withdraw from Heco voting."
-	// 	}
-	// }
-
 
 	function repay() public payable {
 		uint256 repayAmount = msg.value;
@@ -222,34 +190,10 @@ contract Wallet is AccessControl, Global {
 		_revokeAll();
 	}
 
-	// function revokeVotingAndRepay() public {
-	// 	require(msg.sender == _owner);
-	// 	require(isRevokingAllDone() == true, "Revoking vote is in progress...");
-	// 	uint256 amount = _withdrawAllVoting(false);
-	// 
-	// 	require(address(this).balance >= amount);
-	// 	loanContract.repayBehalf{ value: amount }(address(this));
-	// 
-	// 	_withdrawAndBurnHTTFromFilda();
-	// }
-
 	function withdrawAllVoting() public returns (uint256 totalAmount) {
 		require(msg.sender == _owner);
 		return _withdrawAllVoting(false);
 	}
-
-	// function withdraw() public payable {
-	// 	require(msg.sender == _owner);
-	// 
-	// 	uint256 HTBalance = address(this).balance;
-	// 	require(HTBalance > 0);
-	// 
-	// 	uint256 amountToWithdraw = HTBalance.sub(loanContract.borrowBalanceCurrent(address(this)));
-	// 	// 在批量撤回后，一起再把HT转给用户。
-	// 	payable(msg.sender).transfer(amountToWithdraw);
-	// 
-	// 	_withdrawAndBurnHTTFromFilda();
-	// }
 
 	function liquidate() public payable {
 		uint256 borrowBalanceCurrentAmount = loanContract.borrowBalanceCurrent(address(this));
@@ -258,8 +202,6 @@ contract Wallet is AccessControl, Global {
 		require(borrowed > _liquidateRate);
 
 		if (_haveAllVotesBeenRevoked() == false) {
-			// isRevokingAllDone()返回false，说明尚未发起撤回全部投票，则调用_revokeAll(true)去撤回全部投票的全部数量。
-
 			// Step 1: revoke all votes.
 			_revokeAll();
 			_firstLiquidater = msg.sender;
@@ -269,19 +211,15 @@ contract Wallet is AccessControl, Global {
 
 			uint256 total = _withdrawAllVoting(true);
 
-			
 			_secondLiquidater = msg.sender;
 
-
 			uint256 bonus = total.mul(_bonusRateForLiquidater).div(_denominator).div(2);
-			
-			if (_firstLiquidater != _owner)
-				payable(_firstLiquidater).transfer(bonus);
-			if (_secondLiquidater != _owner)
-				payable(_secondLiquidater).transfer(bonus);
+			if (_firstLiquidater != _owner) payable(_firstLiquidater).transfer(bonus);
+			if (_secondLiquidater != _owner) payable(_secondLiquidater).transfer(bonus);
 
 			payable(_admin).transfer(address(this).balance);
-			emit liquidateEvent(address(this), _owner);
+
+			emit liquidateEvent(address(this), total);
 		}
 	}
 
@@ -293,9 +231,6 @@ contract Wallet is AccessControl, Global {
 		emit quickWithdrawalEvent(msg.sender, borrowAmount);
 	}
 
-
-
-
 	// 检查已发起的撤回投票是否完成。
 	// 不存在已发起的撤回投票，以及有未完成的撤回投票，都会返回false。
 	function _haveAllVotesBeenRevoked() private returns (bool allDone) {
@@ -303,7 +238,7 @@ contract Wallet is AccessControl, Global {
 		if (votingDatas.length > 0) {
 			for (uint256 i = 0; i < votingDatas.length; i++) {
 				VotingData memory votedData = votingDatas[i];
-				(uint256 amount, , uint256 lockingEndTime) = revokingInfo(address(this), votedData.pid);
+				(uint256 amount, , uint256 lockingEndTime) = revokingInfo(votedData.pid);
 
 				// TODO: 确认一下，是否只需要读取投票中的HT数量即可，不必同时判断这三个条件？
 				if (lockingEndTime > block.timestamp && isWithdrawable(votedData.pid) && amount < votedData.ballot) {
@@ -315,7 +250,7 @@ contract Wallet is AccessControl, Global {
 		}
 	}
 
-	function _withdrawOrRepay(uint256 pid, bool toRepay) private return (uint256 withdrawal) {
+	function _withdrawOrRepay(uint256 pid, bool toRepay) private returns (uint256 withdrawal) {
 		require(isWithdrawable(pid) == true);
 
 		uint256 oldBalance = address(this).balance;
@@ -323,23 +258,24 @@ contract Wallet is AccessControl, Global {
 		uint256 newBalance = address(this).balance;
 		withdrawal = newBalance.sub(oldBalance);
 
+		uint256 result;
 		if (toRepay) {
-			borrowed = loanContract.borrowBalanceCurrent(msg.sender)
+			uint256 borrowed = loanContract.borrowBalanceCurrent(msg.sender);
+			uint256 repayAmount = borrowed.min(address(this).balance);
 
-			uint256 repayAmount = min(borrowed, address(this).balance);
+			require(loanContract.repayBehalf{ value: repayAmount }(address(this)));
 
-			uint256 result = loanContract.repay(repayAmount);
-			require(result == repayAmount);
-			emit repayEvent(caller, pid, repayAmount);
+			emit repayEvent(msg.sender, pid, repayAmount);
 		}
 
 		result = loanContract.redeemUnderlying(withdrawal);
+
 		require(result == withdrawal);
+
 		_HTT.burn(result);
 
-		emit withdrawEvent(caller, pid, address(this).balance);
+		emit withdrawEvent(msg.sender, pid, address(this).balance);
 	}
-
 
 	// 撤回全部投票的全部量，只供清算时内部调用。
 	function _revokeAll() private returns (bool allDone) {
@@ -367,23 +303,7 @@ contract Wallet is AccessControl, Global {
 		}
 	}
 
-	// 从Filda里提取出已抵押的HTT并销毁。
-	// 
-	// function _withdrawAndBurnHTTFromFilda() public returns (uint256) {
-	// 	uint256 savingBalance = loanContract.getSavingBalance(address(this)).sub(loanContract.borrowBalanceCurrent(address(this)));
-	// 	if (savingBalance > 0) {
-	// 		uint256 result = loanContract.redeemUnderlying(savingBalance);
-	// 		if (result > 0) {
-	// 			if (_HTT.burn(result) == false) {
-	// 				revert(); //"Failed to burn HTT"
-	// 			}
-	// 			return result;
-	// 		} else {
-	// 			revert(); //"Failed to withdraw from Filda"
-	// 		}
-	// 	} else {
-	// 		revert(); //"No deposited HTT."
-	// 	}
-	// }
-	********/
+	function _exchangeRateStored() private view returns (uint256) {
+		return _exchangeRate;
+	}
 }
--- contracts/WalletFactory.sol
+++ contracts/WalletFactory.sol
@@ -5,28 +5,28 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./Wallet.sol";
 
 contract WalletFactory is AccessControl {
-    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+	bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
-    mapping(address => Wallet) wallets;
-    mapping(address => address) users;
+	mapping(address => Wallet) wallets;
+	mapping(address => address) users;
 
-    constructor() {
-        _setupRole(ADMIN_ROLE, msg.sender);
-    }
+	constructor() {
+		_setupRole(ADMIN_ROLE, msg.sender);
+	}
 
-    function makeWallet() public {
-        require(wallets[msg.sender].isExist() == false);
-        Wallet wallet = new Wallet(msg.sender);
-        address walletAddress = address(wallet);
-        wallets[msg.sender] = wallet;
-        users[walletAddress] = msg.sender;
-    }
+	function makeWallet() public {
+		require(wallets[msg.sender].isExist() == false);
+		Wallet wallet = new Wallet(msg.sender, msg.sender);
+		address walletAddress = address(wallet);
+		wallets[msg.sender] = wallet;
+		users[walletAddress] = msg.sender;
+	}
 
-    function getWallet(address owner) public view returns (address) {
-        return address(wallets[owner]);
-    }
+	function getWallet(address owner) public view returns (address) {
+		return address(wallets[owner]);
+	}
 
-    function getOwner(address walletAddress) public view returns (address) {
-        return users[walletAddress];
-    }
+	function getOwner(address walletAddress) public view returns (address) {
+		return users[walletAddress];
+	}
 }
